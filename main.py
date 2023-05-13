40800e894784e0846d09d79aac9ecded3f564782
Rokon Ron <rokon.raz@gmail.com>
2023-05-13 12:54:26 +0600

    Create a entry and label.

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from tkinter import *
 
 window = Tk()
 window.title("Convert Mile to KM")
-window.config(bg="Gray",height=200,width=400)
+window.minsize(height=200, width=400)
+window.config(bg="Gray", height=200, width=400)
+
+
+# Create Entry.
+mile_entry = Entry()
+mile_entry.config(width=20)
+mile_entry.pack()
+
+# Create Label.
+mile_label = Label()
+mile_label.config(text="Mile", bg="Gray", font=("Arial", 10, "bold"))
+mile_label.pack()
 
 
 window.mainloop()
