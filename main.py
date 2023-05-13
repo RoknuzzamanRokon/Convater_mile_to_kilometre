cf403d164f739e551d6ace985c3440725bbfcdd3
Rokon Ron <rokon.raz@gmail.com>
2023-05-13 13:00:56 +0600

    Add some text and entryBox.

--- main.py
+++ main.py
@@ -7,14 +7,27 @@ window.config(bg="Gray", height=200, width=400)
 
 
 # Create Entry.
-mile_entry = Entry()
-mile_entry.config(width=20)
-mile_entry.pack()
+mile_entry01 = Entry()
+mile_entry01.config(width=20)
+mile_entry01.pack()
 
 # Create Label.
 mile_label = Label()
 mile_label.config(text="Mile", bg="Gray", font=("Arial", 10, "bold"))
 mile_label.pack()
+
+my_label_01 = Label()
+my_label_01.config(text="is equal ", bg="Gray", font=("Arial", 10, "bold"))
+my_label_01.pack()
+
+
+mile_entry02 = Entry()
+mile_entry02.config(width=20)
+mile_entry02.pack()
+
+my_label_02 = Label()
+my_label_02.config(text="KM ", bg="Gray", font=("Arial", 10, "bold"))
+my_label_02.pack()
 
 
 window.mainloop()
