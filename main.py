76a3b7f8358aa0c6268503e6dc00a5a96f31b7c1
Rokon Ron <rokon.raz@gmail.com>
2023-05-13 14:18:05 +0600

    Decorate GUI in program using 'grid' method

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import *
 window = Tk()
 window.title("Convert Mile to KM")
 window.minsize(height=200, width=400)
-window.config(bg="Gray", height=200, width=400)
+window.config(bg="Gray", pady=50, padx=100)
 
 
 # Create a button function.
@@ -13,35 +13,41 @@ def button_function():
     my_textbox.insert(END, convert_file)
 
 
-
 # Create Entry.
 mile_entry01 = Entry()
 mile_entry01.config(width=20)
-mile_entry01.pack()
+mile_entry01.grid(row=1, column=2)
+# mile_entry01.pack()
 
 # Create Label.
 mile_label = Label()
 mile_label.config(text="Mile", bg="Gray", font=("Arial", 10, "bold"))
-mile_label.pack()
+mile_label.grid(row=1, column=3)
+# mile_label.pack()
 
 my_label_01 = Label()
 my_label_01.config(text="is equal ", bg="Gray", font=("Arial", 10, "bold"))
-my_label_01.pack()
+my_label_01.grid(row=2, column=1)
+# my_label_01.pack()
 
 # Create a textBox.
 my_textbox = Text()
 my_textbox.config(width=10, height=1)
-my_textbox.pack()
+my_textbox.grid(row=2, column=2)
+# my_textbox.pack()
+
 
 my_label_02 = Label()
 my_label_02.config(text="KM ", bg="Gray", font=("Arial", 10, "bold"))
-my_label_02.pack()
+my_label_02.grid(row=2, column=3)
+# my_label_02.pack()
 
 
 # Create a Button.
 my_button = Button()
 my_button.config(text="Calculate", command=button_function)
-my_button.pack()
+my_button.grid(row=3, column=2)
+# my_button.pack()
 
 
 window.mainloop()
