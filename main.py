5ca26c925124c78e7ddea018198cf450410451c5
Rokon Ron <rokon.raz@gmail.com>
2023-05-13 14:05:09 +0600

    edit button function and text box

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ window.config(bg="Gray", height=200, width=400)
 
 
 # Create a button function.
-def button_function(value):
-    convert_file = value * 1.60934
-    return convert_file
+def button_function():
+    flot_miles = float(mile_entry01.get())
+    convert_file = flot_miles * 1.60934
+    my_textbox.insert(END, convert_file)
+
 
 
 # Create Entry.
@@ -26,10 +28,10 @@ my_label_01 = Label()
 my_label_01.config(text="is equal ", bg="Gray", font=("Arial", 10, "bold"))
 my_label_01.pack()
 
-
-mile_entry02 = Entry()
-mile_entry02.config(width=20)
-mile_entry02.pack()
+# Create a textBox.
+my_textbox = Text()
+my_textbox.config(width=10, height=1)
+my_textbox.pack()
 
 my_label_02 = Label()
 my_label_02.config(text="KM ", bg="Gray", font=("Arial", 10, "bold"))
@@ -38,7 +40,7 @@ my_label_02.pack()
 
 # Create a Button.
 my_button = Button()
-my_button.config(text="Calculate")
+my_button.config(text="Calculate", command=button_function)
 my_button.pack()
 
 
